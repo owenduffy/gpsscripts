5e6740341f006411dfc5e3c4aac3e52ab210bc43
owen <owen@owenduffy.net>
2019-10-15 09:25:40 +1100

    Initial.

---
+++ nmea2kmlkmzgpx.bat
@@ -0,0 +1,30 @@
+@echo off
+if *%1==* goto usage
+
+set ERROR=0.005k
+set FILE=%1
+set NAME=%~d1%~p1%~n1
+set DIR=%~d1%~p1
+set colour=ffbf00bf
+if not *%2==* set colour=%2
+set GPSBABEL="D:\Program Files (x86)\GPSBabel\GPSBabel"
+set ZIP="s:\7zip\7za.exe"
+echo working, may take a while...
+%GPSBABEL%  -i nmea -f %FILE% -x simplify,error=%ERROR% -x transform,rte=trk,del -x discard,fixnone,fixunknown -o kml,track=1,points=0,labels=0,line_color=%colour%,line_width=3 -F "%NAME%.kml" -o gpx -F "%NAME%.gpx"
+rem %GPSBABEL%  -i nmea,ignore_fix=1 -f %FILE% -x simplify,error=%ERROR% -x transform,rte=trk,del -o kml,track=1,points=0,labels=0,line_color=%colour%,line_width=3 -F "%NAME%.kml" -o gpx -F "%NAME%.gpx"
+if not exist %ZIP% goto end
+del /q "%DIR%doc.kml"
+del /q "%NAME%.kmz"
+copy "%NAME%.kml" "%DIR%doc.kml"
+echo %ZIP% a -tzip "%NAME%.kmz" "%DIR%doc.kml"
+%ZIP% a -tzip "%NAME%.kmz" "%DIR%doc.kml"
+rem del /q "%DIR%doc.kml"
+goto end
+
+:usage
+echo nmea2kml filename [color]
+
+:end
+pause
+
+
